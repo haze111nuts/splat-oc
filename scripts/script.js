c5b39e8ac98a0ae5bf55d9c8643f4114325d59b4
haze111nuts <hasami75@gmail.com>
2023-07-02 16:11:07 -0400

    minor ui text change: key triat to theme

--- scripts/script.js
+++ scripts/script.js
@@ -248,7 +248,7 @@ function setUpBioPageFor(charaBioData) {
     bioBasicHTML += "<tr><td>Mains</td><td>" + charaBioData.mains + "</td></tr>";
     bioBasicHTML += "</table>";
 
-    var bioQuirkHTML = "<div class='sectionLabel'>Key Trait</div>";
+    var bioQuirkHTML = "<div class='sectionLabel'>Theme</div>";
     bioQuirkHTML += "<ul class='keyTrait'><li>"+charaBioData.quirk+"</li></ul>";
 
     var bioTraitsHTML = "<div class='sectionLabel'>Traits</div>";
